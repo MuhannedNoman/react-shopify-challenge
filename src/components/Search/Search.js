420a385b54f5b63aafffabb2ba36ea35bafea900
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-08-24 17:59:04 -0700

    add some style to the search bar

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,20 +1,19 @@
-import React, { useState } from "react";
-import { Form } from "react-bootstrap";
-function Search({ setQuery }) {
-  const [search, setSearch] = useState("");
-  function handleSearch(e) {
-    setSearch(e.target.value);
-    setQuery(search);
-  }
+import React from "react";
+
+// Styles
+import { SearchWrapperStyles } from "./Style";
+
+function Search({ handleSearch }) {
+  //setup before functions
+
   return (
     <div>
-      <h3>Search For Movies</h3>
       <br />
-      <Form.Control
+      <SearchWrapperStyles
         type="text"
-        placeholder="Movie name"
+        placeholder="&#xF002; Movie name"
         name="search"
-        onChange={handleSearch}
+        onBlur={handleSearch}
       />
     </div>
   );
