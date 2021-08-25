8996530e86aa6398296e531e252eebd7286dd6e7
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-08-24 17:58:42 -0700

    refactor movie list

--- src/components/MovieList/Style.js
+++ src/components/MovieList/Style.js
@@ -3,4 +3,5 @@ import styled from "styled-components";
 export const MoviesDataWrapper = styled.div`
   display: flex;
   justify-content: center;
+  margin: 10px;
 `;
---
+++ src/components/MovieList/index.js
@@ -0,0 +1,99 @@
+import React, { useState } from "react";
+import Search from "../Search/Search";
+// hooks
+import useMovieFetch from "../hooks/useMovieFetch";
+
+// @material-ui
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+
+// Styles
+import { MoviesDataWrapper } from "./Style";
+//@material-ui
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 140,
+  },
+});
+const MovieList = () => {
+  const [search, setSearch] = useState("");
+
+  function handleSearch(e) {
+    setSearch(e.target.value);
+  }
+
+  // @material-ui useStyles
+  const classes = useStyles();
+
+  // Fetch data for a movies
+  const { data, isLoaded } = useMovieFetch(
+    `http://www.omdbapi.com/?t=${search}&apikey=780560ff`
+  );
+  if (!isLoaded) return <p>Loading ..</p>;
+
+  // Destructuring data object
+  const {
+    Actors,
+    Awards,
+    DVD,
+    Director,
+    Genre,
+    Language,
+    Metascore,
+    Rated,
+    Released,
+    Title,
+    Writer,
+    Poster,
+  } = data;
+  return (
+    <>
+      <Search search={search} handleSearch={handleSearch} />
+      <MoviesDataWrapper>
+        <Card className={classes.root}>
+          <CardActionArea>
+            <CardMedia
+              className={classes.media}
+              src={Poster}
+              alt={Title}
+              title={Title}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                {Title}
+              </Typography>
+              <Typography variant="body2" color="textSecondary" component="p">
+                <p>Actors: {Actors} </p>
+                <p>Language: {Language}</p>
+                <p>Genre: {Genre}</p>
+                <p>Awards: {Awards}</p>
+                <p>Metascore: {Metascore}</p>
+                <p>DVD: {DVD}</p>
+                <p>Rated: {Rated}</p>
+                <p>Released: {Released}</p>
+                <p>Writer: {Writer}</p>
+                <p>Director: {Director}</p>
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+          <CardActions>
+            <Button size="small" color="primary">
+              Nominate
+            </Button>
+          </CardActions>
+        </Card>
+      </MoviesDataWrapper>
+    </>
+  );
+};
+
+export default MovieList;
