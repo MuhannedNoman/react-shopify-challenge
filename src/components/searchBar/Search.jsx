d40b989cb82407075e5408050f56ce677a722969
Haider <haiderlek@gmail.com>
2021-08-26 18:40:15 +0300

    feat(SearchBar): add recommended options (search auto complete) recommend 4 titles that include the search term

--- src/components/searchBar/Search.jsx
+++ src/components/searchBar/Search.jsx
@@ -3,21 +3,36 @@ import moviesContext, { ACTIONS } from "../../store/moviesContext";
 
 function Search() {
   const { dispatchMovies, moviesState } = useContext(moviesContext);
+  const { searchValue, movies } = moviesState;
   function handleSearch(e) {
     dispatchMovies({
       type: ACTIONS.SEARCH,
       payload: e.target.value.trim(),
     });
   }
-
+  const moviesOptions = movies.slice(0, 4).filter((movie) => {
+    return movie.Title.toLowerCase()
+      .trim()
+      .includes(searchValue.toLowerCase().trim());
+  });
   return (
     <div>
       <h1>Search</h1>
       <input
-        value={moviesState.searchValue}
+        list="search"
+        value={searchValue}
         onChange={handleSearch}
         type="text"
       />
+      <datalist id="search">
+        {moviesOptions.map((mov) => {
+          return (
+            <option key={mov.imdbID} value={mov.Title}>
+              {mov.Title}
+            </option>
+          );
+        })}
+      </datalist>
     </div>
   );
 }
