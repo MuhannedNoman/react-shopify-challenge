00f7ea5a1bad267977b9d496d9c0ba24f9897e5d
Haider <haiderlek@gmail.com>
2021-08-26 14:47:56 +0300

    feat(MoviesList): send the fetched data with "movies" type to the store.
    used dispatchMovies to store the movies in the context after fetching.

--- src/components/movies/MoviesList.jsx
+++ src/components/movies/MoviesList.jsx
@@ -1,13 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import useFetch from "../../hooks/useFetch";
-import moviesContext from "../../store/moviesContext";
+import moviesContext, { ACTIONS } from "../../store/moviesContext";
 
 function MoviesList() {
-  const { moviesState } = useContext(moviesContext);
+  const { moviesState, dispatchMovies } = useContext(moviesContext);
   const { data } = useFetch(
     `https://www.omdbapi.com/?s=${moviesState.searchValue}&apikey=18415e4d`
   );
-  data;
+  useEffect(() => {
+    if (data.Search)
+      dispatchMovies({
+        type: ACTIONS.MOVIES,
+        payload: data.Search.filter((mov) => {
+          return mov.Type === "movie";
+        }),
+      });
+    else
+      dispatchMovies({
+        type: ACTIONS.MOVIES,
+        payload: [],
+      });
+  }, [data.Search]);
 
   return <div></div>;
 }
