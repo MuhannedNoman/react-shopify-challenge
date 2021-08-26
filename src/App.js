9134ce964f264319cd62d156ecde990e576526b9
Haider <haiderlek@gmail.com>
2021-08-26 17:35:27 +0300

    feat(NominatesList): fetch movies by ID
    use the custom hook to fetch movies by id to get more information about the movie

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import Search from "./components/searchBar/Search";
 import MoviesList from "./components/movies/MoviesList";
+import NominatesList from "./components/movies/NominatesList";
 
 function App() {
   return (
     <div className="App">
       <Search />
       <MoviesList />
+      <NominatesList />
     </div>
   );
 }
---
+++ src/components/movies/NominatesList.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import useFetch from "../../hooks/useFetch";
+import moviesContext from "../../store/moviesContext";
+
+function NominatesList() {
+  const { moviesState } = useContext(moviesContext);
+  const { nominatedID } = moviesState;
+
+  const { data } = useFetch(
+    `http://www.omdbapi.com/?i=${nominatedID}&apikey=18415e4d`
+  );
+  data;
+
+  return <div></div>;
+}
+
+export default React.memo(NominatesList);
