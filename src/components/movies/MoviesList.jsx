35543b954c4064f67908002167fc4d8a53cea687
Haider <haiderlek@gmail.com>
2021-08-26 15:10:30 +0300

    feat(MoviesList): map over the first movies and display them to the user. sliced the first 3 movies to display them with the Movie component

--- src/components/movies/MoviesList.jsx
+++ src/components/movies/MoviesList.jsx
@@ -1,9 +1,11 @@
 import React, { useContext, useEffect } from "react";
 import useFetch from "../../hooks/useFetch";
 import moviesContext, { ACTIONS } from "../../store/moviesContext";
+import Movie from "./Movie";
 
 function MoviesList() {
   const { moviesState, dispatchMovies } = useContext(moviesContext);
+  const { movies } = moviesState;
   const { data } = useFetch(
     `https://www.omdbapi.com/?s=${moviesState.searchValue}&apikey=18415e4d`
   );
@@ -22,7 +24,13 @@ function MoviesList() {
       });
   }, [data.Search]);
 
-  return <div></div>;
+  return (
+    <div>
+      {movies.slice(0, 3).map((movie) => {
+        return <Movie key={movie.imdbID} movie={movie} />;
+      })}
+    </div>
+  );
 }
 
 export default MoviesList;
