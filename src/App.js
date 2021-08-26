07a77a79829d0ff374ce6db62b7233af0f578726
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-08-25 20:44:53 -0700

    move the nominates component to the app

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import { Col, Row } from "react-bootstrap";
 
 // Styles
 import { AppStyles } from "./AppStyle";
-import NominatedList from "./components/NominatedList";
+import Nominates from "./components/MovieList/Nominates";
+import NominateProvider from "./components/Store/nominateStore";
 
 function App() {
   return (
     <AppStyles>
-      <Row>
-        <Col lg="6" md="12">
-          <MovieList />
-        </Col>
-        <Col lg="6" md="12">
-          <NominatedList />
-        </Col>
-      </Row>
+      <NominateProvider>
+        <Row>
+          <Col lg="6" md="12">
+            <MovieList />
+          </Col>
+          <Col lg="6" md="12">
+            <Nominates />
+          </Col>
+        </Row>
+      </NominateProvider>
     </AppStyles>
-
   );
 }
 
