e640fff73ff6c77801f61b011ee2f0464f1c6fc3
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-08-25 17:55:46 -0700

    add layout to the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import MovieList from "./components/MovieList";
+import { Col, Row } from "react-bootstrap";
+
+// Styles
+import { AppStyles } from "./AppStyle";
+import NominatedList from "./components/NominatedList";
+
 function App() {
   return (
-    <div>
-      <MovieList />
-    </div>
+    <AppStyles>
+      <Row>
+        <Col lg="6" md="12">
+          <MovieList />
+        </Col>
+        <Col lg="6" md="12">
+          <NominatedList />
+        </Col>
+      </Row>
+    </AppStyles>
   );
 }
 
