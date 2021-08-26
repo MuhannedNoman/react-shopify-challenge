717bc0173b35071b384f16da0d757c349b2cc7f7
Haider <haiderlek@gmail.com>
2021-08-26 15:17:58 +0300

    feat(Movie): add the functionality to nominate movies on button click send the nominated movies to the moviesContext store by dispatching

--- src/components/movies/Movie.jsx
+++ src/components/movies/Movie.jsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
+import moviesContext, { ACTIONS } from "../../store/moviesContext";
 
 function Movie({ movie }) {
+  const { dispatchMovies } = useContext(moviesContext);
+  function onNominate(movie) {
+    dispatchMovies({
+      type: ACTIONS.NOMINATES,
+      payload: movie,
+    });
+  }
+
   return (
     <div>
       <h3>{movie.Title}</h3>
       <h4>{movie.Year}</h4>
       <img src={movie.Poster} alt="" />
-      <button>Nominate</button>
+      <button onClick={() => onNominate(movie)}>Nominate</button>
     </div>
   );
 }
