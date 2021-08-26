022d70314bd22ac6cf2bd4cccde223587f2e1e4a
Haider <haiderlek@gmail.com>
2021-08-26 15:28:55 +0300

    feat(Movie): disable nominate button if already nominated if the movie is already in the nominates list, show a disabled nominated button so we can nominate each movie only once

--- src/components/movies/Movie.jsx
+++ src/components/movies/Movie.jsx
@@ -2,20 +2,28 @@ import React, { useContext } from "react";
 import moviesContext, { ACTIONS } from "../../store/moviesContext";
 
 function Movie({ movie }) {
-  const { dispatchMovies } = useContext(moviesContext);
+  const { dispatchMovies, moviesState } = useContext(moviesContext);
+  const { nominates } = moviesState;
   function onNominate(movie) {
     dispatchMovies({
       type: ACTIONS.NOMINATES,
       payload: movie,
     });
   }
+  const isNominated = nominates.find((mov) => {
+    return mov.imdbID === movie.imdbID;
+  });
 
   return (
     <div>
       <h3>{movie.Title}</h3>
       <h4>{movie.Year}</h4>
       <img src={movie.Poster} alt="" />
-      <button onClick={() => onNominate(movie)}>Nominate</button>
+      {!isNominated ? (
+        <button onClick={() => onNominate(movie)}>Nominate</button>
+      ) : (
+        <button disabled>Nominated</button>
+      )}
     </div>
   );
 }
