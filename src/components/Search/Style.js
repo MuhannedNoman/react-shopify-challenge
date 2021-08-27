2fcf2e78bda232a883e707a05216f95b5d0a90e3
Muhammed <muhammadburhan964@gmail.com>
2021-08-27 17:41:42 +0300

    Fixed some styling

--- src/components/Search/Style.js
+++ src/components/Search/Style.js
@@ -4,6 +4,6 @@ import { Form } from "react-bootstrap";
 export const SearchWrapperStyles = styled(Form.Control)`
   width: 400px;
   padding: 10px;
-  margin: auto;
-  display: block;
+  margin: 10px;
+  display: inline-block;
 `;
