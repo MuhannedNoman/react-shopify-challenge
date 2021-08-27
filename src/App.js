4ddf8e3a3eed630b0430109be5aff6d5212d2e28
Haider <haiderlek@gmail.com>
2021-08-27 11:55:34 +0300

    feat(Modal): display a random movie when nominates = 5 whenever the user nominates 5 movies display a modal with the winner

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
-import React from "react";
+import React, { useContext } from "react";
 import Search from "./components/searchBar/Search";
 import MoviesList from "./components/movies/MoviesList";
 import NominatesList from "./components/movies/NominatesList";
+import moviesContext from "./store/moviesContext";
+import Modal from "./components/modal/Modal";
 
 function App() {
+  const { moviesState } = useContext(moviesContext);
+  const { nominates } = moviesState;
   return (
     <div className="App">
       <Search />
       <MoviesList />
       <NominatesList />
+      {nominates.length === 5 && <Modal />}
     </div>
   );
 }
--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import moviesContext from "../../store/moviesContext";
 
 function Modal() {
-  return <div></div>;
+  const { moviesState } = useContext(moviesContext);
+  const { nominates } = moviesState;
+  const randomMovie = nominates[Math.floor(Math.random() * nominates.length)];
+  return (
+    <div>
+      <h1>{randomMovie.Title} Won!</h1>
+    </div>
+  );
 }
 
 export default Modal;
