b918f5f06558324033d4528803a8e52c509197f1
Haider <haiderlek@gmail.com>
2021-08-27 11:58:22 +0300

    feat(Modal): add a reset button reset the movies and the search input on click

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,13 +1,28 @@
 import React, { useContext } from "react";
-import moviesContext from "../../store/moviesContext";
+import moviesContext, { ACTIONS } from "../../store/moviesContext";
 
 function Modal() {
-  const { moviesState } = useContext(moviesContext);
+  const { moviesState, dispatchMovies } = useContext(moviesContext);
   const { nominates } = moviesState;
   const randomMovie = nominates[Math.floor(Math.random() * nominates.length)];
+  function reset() {
+    dispatchMovies({
+      type: ACTIONS.SEARCH,
+      payload: "",
+    });
+    dispatchMovies({
+      type: ACTIONS.NOMINATE_REMOVE,
+      payload: [],
+    });
+    dispatchMovies({
+      type: ACTIONS.MOVIES,
+      payload: [],
+    });
+  }
   return (
     <div>
       <h1>{randomMovie.Title} Won!</h1>
+      <button onClick={reset}>Restart</button>
     </div>
   );
 }
